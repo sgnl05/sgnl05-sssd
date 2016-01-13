b992cf315f0adf1563f5df007799fa88f41caf0a
curly8 <curly8@users.noreply.github.com>
2016-01-13 15:01:46 +0100

    Line is too long fix

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,8 +11,9 @@ describe 'sssd' do
       it { is_expected.to contain_package('authconfig').with_ensure('latest') }
       it { is_expected.not_to contain_package('oddjob-mkhomedir') }
       it { is_expected.not_to contain_service('oddjobd') }
-      it { should contain_exec('authconfig-mkhomedir') \
-      .that_comes_before('File[sssd.conf]') }
+      it 'should pass' do
+        contain_exec('authconfig-mkhomedir').that_comes_before('File[sssd.conf]')
+      end
 
       it { is_expected.to contain_service('sssd').with_ensure('running') }
     end
@@ -33,8 +34,9 @@ describe 'sssd' do
       it { is_expected.to contain_package('authconfig').with_ensure('present') }
       it { is_expected.to contain_package('oddjob-mkhomedir') }
       it { is_expected.to contain_service('oddjobd') }
-      it { should contain_exec('authconfig-mkhomedir') \
-      .that_comes_before('File[sssd.conf]') }
+      it 'should pass' do
+        contain_exec('authconfig-mkhomedir').that_comes_before('File[sssd.conf]')
+      end
 
       it { is_expected.to contain_service('sssd').with_ensure('running') }
       it { is_expected.to contain_service('oddjobd').with_ensure('running') }
