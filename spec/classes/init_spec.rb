c36a3daf054927a0c8bc5995618a245ea5fb867d
Brad Cowie <brad@gizmoguy.net.nz>
2016-01-13 15:12:32 +1300

    Stop sorting config keys for newer ruby versions.
     * For ruby versions > 1.9.3 use order specified by user for
       configuration, otherwise sort it alphabetically.
     * Remove extraneous whitespace.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,22 +1,25 @@
 require 'spec_helper'
 describe 'sssd' do
   describe 'on RedHat 5.11' do
-    let(:facts) { {
-      :osfamily => 'RedHat',
-      :operatingsystemrelease => '5.7',
-      :rubyversion => '1.9.3'
-    } }
+    let(:facts) do
+      {
+        :osfamily => 'RedHat',
+        :operatingsystemrelease => '5.7',
+        :rubyversion => '1.9.3'
+      }
+    end
 
     context 'with defaults for all parameters' do
       it { is_expected.to contain_class('sssd::install') }
       it { is_expected.to contain_class('sssd::config') }
       it { is_expected.to contain_class('sssd::service') }
 
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
-      }
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
+      end
 
       it { is_expected.to contain_package('authconfig').with_ensure('latest') }
       it { is_expected.not_to contain_package('oddjob-mkhomedir') }
@@ -32,35 +35,41 @@ describe 'sssd' do
     end
 
     context 'with ruby without ordered hashes' do
-      let(:facts) { {
-        :osfamily => 'RedHat',
-        :operatingsystemrelease => '5.7',
-        :rubyversion => '1.8.7'
-      } }
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
-      }
+      let(:facts) do
+        {
+          :osfamily => 'RedHat',
+          :operatingsystemrelease => '5.7',
+          :rubyversion => '1.8.7'
+        }
+      end
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
+      end
     end
   end
   describe 'on RedHat 6.6' do
-    let(:facts) { {
-      :osfamily => 'RedHat',
-      :operatingsystemrelease => '6.6',
-      :rubyversion => '1.9.3'
-    } }
+    let(:facts) do
+      {
+        :osfamily => 'RedHat',
+        :operatingsystemrelease => '6.6',
+        :rubyversion => '1.9.3'
+      }
+    end
 
     context 'with defaults for all parameters' do
       it { is_expected.to contain_class('sssd::install') }
       it { is_expected.to contain_class('sssd::config') }
       it { is_expected.to contain_class('sssd::service') }
 
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
-      }
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
+      end
 
       it { is_expected.to contain_package('authconfig').with_ensure('present') }
       it { is_expected.to contain_package('oddjob-mkhomedir') }
@@ -78,35 +87,41 @@ describe 'sssd' do
     end
 
     context 'with ruby without ordered hashes' do
-      let(:facts) { {
-        :osfamily => 'RedHat',
-        :operatingsystemrelease => '6.6',
-        :rubyversion => '1.8.7'
-      } }
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
-      }
+      let(:facts) do
+        {
+          :osfamily => 'RedHat',
+          :operatingsystemrelease => '6.6',
+          :rubyversion => '1.8.7'
+        }
+      end
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
+      end
     end
   end
   describe 'on RedHat 7.1' do
-    let(:facts) { {
-      :osfamily => 'RedHat',
-      :operatingsystemrelease => '7.1',
-      :rubyversion => '1.9.3'
-    } }
+    let(:facts) do
+      {
+        :osfamily => 'RedHat',
+        :operatingsystemrelease => '7.1',
+        :rubyversion => '1.9.3'
+      }
+    end
 
     context 'with defaults for all parameters' do
       it { is_expected.to contain_class('sssd::install') }
       it { is_expected.to contain_class('sssd::config') }
       it { is_expected.to contain_class('sssd::service') }
 
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
-      }
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
+      end
 
       it { is_expected.to contain_package('authconfig').with_ensure('present') }
       it { is_expected.to contain_package('oddjob-mkhomedir') }
@@ -124,35 +139,41 @@ describe 'sssd' do
     end
 
     context 'with ruby without ordered hashes' do
-      let(:facts) { {
-        :osfamily => 'RedHat',
-        :operatingsystemrelease => '7.1',
-        :rubyversion => '1.8.7'
-      } }
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
-      }
+      let(:facts) do
+        {
+          :osfamily => 'RedHat',
+          :operatingsystemrelease => '7.1',
+          :rubyversion => '1.8.7'
+        }
+      end
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
+      end
     end
   end
   describe 'on Debian 8.1' do
-    let(:facts) { {
-      :osfamily => 'Debian',
-      :operatingsystemrelease => '8.1',
-      :rubyversion => '1.9.3'
-    } }
+    let(:facts) do
+      {
+        :osfamily => 'Debian',
+        :operatingsystemrelease => '8.1',
+        :rubyversion => '1.9.3'
+      }
+    end
 
     context 'with defaults for all parameters' do
       it { is_expected.to contain_class('sssd::install') }
       it { is_expected.to contain_class('sssd::config') }
       it { is_expected.to contain_class('sssd::service') }
 
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
-      }
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[sssd\]/)
+      end
 
       it { is_expected.not_to contain_package('authconfig') }
       it { is_expected.not_to contain_package('oddjob-mkhomedir') }
@@ -168,16 +189,19 @@ describe 'sssd' do
     end
 
     context 'with ruby without ordered hashes' do
-      let(:facts) { {
-        :osfamily => 'Debian',
-        :operatingsystemrelease => '8.1',
-        :rubyversion => '1.8.7'
-      } }
-      it { is_expected.to contain_file('sssd.conf') \
-        .with_ensure('present') \
-        .with_path('/etc/sssd/sssd.conf') \
-        .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
-      }
+      let(:facts) do
+        {
+          :osfamily => 'Debian',
+          :operatingsystemrelease => '8.1',
+          :rubyversion => '1.8.7'
+        }
+      end
+      it do
+        is_expected.to contain_file('sssd.conf') \
+          .with_ensure('present') \
+          .with_path('/etc/sssd/sssd.conf') \
+          .with_content(/^# Managed by Puppet.\n\n\[domain\/ad.example.com\]/)
+      end
     end
   end
 end
