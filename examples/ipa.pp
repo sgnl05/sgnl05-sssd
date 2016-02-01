36d9bea65ecc704e83f6ea5a95b14aacc3f1281a
sgnl05 <gjerjens@gmail.com>
2016-02-01 19:52:43 +0100

    Example configurations

---
+++ examples/ipa.pp
@@ -0,0 +1,20 @@
+class {'::sssd':
+  config => {
+    'sssd' => {
+      'domains'             => 'example.com',
+      'config_file_version' => 2,
+      'services'            => ['nss', 'sudo', 'pam', 'ssh'],
+    },
+    'domain/ad.example.com' => {
+      'ipa_domain'                     => 'example.com',
+      'ipa_server'                     => ['ipaserver1.example.com', 'ipaserver2.example.com'],
+      'ipa_hostname'                   => 'client1.example.com',
+      'id_provider'                    => 'ipa',
+      'access_provider'                => 'ipa',
+      'chpass_provider'                => 'ipa'
+      'cache_credentials'              => true,
+      'krb5_store_password_if_offline' => true,
+      'ldap_tls_cacert'                => '/etc/ipa/ca.crt',
+    }
+  }
+}
