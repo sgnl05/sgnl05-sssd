1e1f25c57703c9006916ce3e02e4cbbbd7ff033d
Gjermund Jensvoll <sgnl05@users.noreply.github.com>
2016-02-05 19:15:37 +0100

    ldap example

---
+++ examples/ldap.pp
@@ -0,0 +1,19 @@
+class {'::sssd':
+  config => {
+    'sssd' => {
+      'domains'             => 'example.com',
+      'config_file_version' => 2,
+      'services'            => ['nss', 'pam'],
+    },
+    'domain/example.com' => {
+      'id_provider'           => 'ldap',
+      'auth_provider'         => 'ldap',
+      'cache_credentials'     => true,
+      'ldap_uri'              => 'ldap://ldap.example.com',
+      'ldap_search_base'      => 'dc=example,dc=com',
+      'ldap_id_use_start_tls' => true,
+      'ldap_tls_reqcert'      => 'demand',
+      'ldap_tls_cacert'       => '/etc/pki/tls/certs/ca-bundle.crt',
+    }
+  }
+}
