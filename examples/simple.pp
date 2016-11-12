002b3c5d345da531c23b122d83bf63f64f09e336
gjermund <gjerjens@gmail.com>
2016-11-12 22:20:16 +0100

    Example for SSSD's simple access-control provider

---
+++ examples/simple.pp
@@ -0,0 +1,13 @@
+class {'::sssd':
+  config => {
+    'sssd'               => {
+      'domains'             => 'example.com',
+      'config_file_version' => 2,
+      'services'            => ['nss', 'pam'],
+    },
+    'domain/example.com' => {
+      'access_provider'    => 'simple',
+      'simple_allow_users' => ['user1', 'user2'],
+    }
+  }
+}
