6743957f08786b9d67e2028e1074342b3513d12a
Marc Chadwick <marc@chadwick.net>
2017-02-22 16:20:27 -0500

    * Manually fixed formatting issues Travis was complaining about

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,14 +2,14 @@
 class sssd::service (
   $sssd_service         = $sssd::sssd_service,
   $service_ensure       = $sssd::ensure ? {
-    absent      => 'stopped',
+    'absent'      => 'stopped',
     default     => $sssd::service_ensure,
-    }
+  }
 ) {
 
   $service_enable = $service_ensure ? {
-      stopped    => false,
-      default   => true,
+    'stopped'    => false,
+    default   => true,
   }
   ensure_resource('service', $sssd_service,
     {
