1e479a0163dfa18f50ca95c37fd5d295baf2da80
Laura Hild <lsh@jlab.org>
2023-03-01 15:16:04 -0500

    Claim to support thru EL9

--- manifests/init.pp
+++ manifests/init.pp
@@ -97,8 +97,8 @@ class sssd (
         warning("osname Amazon's os.release.major is <${::facts['os']['release']['major']}> and must be 2.")
       }
     } elsif ($::facts['os']['name'] in ['RedHat', 'CentOS']) {
-      if !($::facts['os']['release']['major'] in ['6', '7', '8']) {
-        warning("osname RedHat's os.release.major is <${::facts['os']['release']['major']}> and must be 6, 7 or 8.")
+      if !($::facts['os']['release']['major'] in ['6', '7', '8', '9']) {
+        warning("osname RedHat's os.release.major is <${::facts['os']['release']['major']}> and must be 6, 7, 8, or 9.")
       }
     } elsif ($::facts['os']['name'] == 'Fedora') {
       if !($::facts['os']['release']['major'] in ['30', '31', '32', '33']) {
